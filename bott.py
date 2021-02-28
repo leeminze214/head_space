38b8c286ea7d2ee2e598d48dcf56756f608250b2
leeminze214 <leeminz214@gmail.com>
2021-02-28 16:02:32 -0500

    'pingpong'

--- bott.py
+++ bott.py
@@ -1,8 +1,8 @@
 import discord
-import time
 from discord.ext import commands,tasks
 
-bot = commands.Bot(command_prefix='$')
+
+bot = commands.Bot(command_prefix='.', help_command = None)
 water_id = 815637250527592491
 
 @tasks.loop(hours=1.0)
@@ -14,11 +14,14 @@ async def water():
 async def on_ready():
     print('logged in as {0.user}'.format(bot))
     water.start()
+    
 @bot.command()
 async def test(ctx):
     await ctx.send('hi')
     
-  
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency,2)}ms.')
 
 bot.run('ODE1MzI2ODk3NzAxMTI2MTg0.YDqyWA.981jNBre2JTDZ19h0RcHLJuflq4',bot=True)
 
