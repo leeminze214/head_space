c3b96f07699634f53e35044fd638346807f5d979
leeminze214 <leeminz214@gmail.com>
2021-03-09 23:20:58 -0500

    db directory

---
+++ db/methods.py
@@ -0,0 +1,12 @@
+import psycopg2
+#from config import configuration
+
+#params = configuration()
+#con = psycopg2.connect(**params)
+
+#print("Database opened successfully")
+
+#cur = con.cursor()
+#cur.execute(xxx)
+#con.commit()
+#con.close()
