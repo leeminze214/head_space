18ad41fa10f78c60b7c585f81b0d7647e5647980
leeminze214 <leeminz214@gmail.com>
2021-04-02 17:31:18 -0400

    rename valid_addictions to refers

--- custom/refers.py
+++ custom/refers.py
@@ -5,3 +5,5 @@ columns = self.all_addictions()
 valid_addictions = [column[0] for column in columns if column[0] != 's' or column != 'id']
 '''
 valid_addictions = ['gaming','tobacco']
+
+time_names = ['Day','Hour','Minute','Second']
