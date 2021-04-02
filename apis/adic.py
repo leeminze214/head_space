63dd3493be21e78a6ba42ae8604f2c20326c4c4c
leeminze214 <leeminz214@gmail.com>
2021-04-02 19:54:25 -0400

    separate apis

---
+++ apis/adic.py
@@ -0,0 +1,169 @@
+import datetime as dt
+from db.methods import methods
+from custom.refers import valid_addictions
+
+
+class addictions:
+##
+    def count_users_addicted(self, addiction):
+        '''
+            fetches number of users with
+             <addiction> addiction
+        '''
+
+        query = f'''
+                    SELECT COUNT({addiction}) FROM user_addictions
+                        WHERE {addiction} IS NOT NULL;
+                '''
+        res = methods.basic_fetch(query,'one')[0]
+        return res
+    
+
+##
+    def add_addiction(self, user_id, addiction):
+        '''
+            inserts addiction into user 
+            account to keep track of
+        '''
+
+        time_now = dt.datetime.now()
+        res = self.fetch_addiction(user_id,addiction)
+        if res is None:
+            update_query = f'''
+                            UPDATE user_addictions
+                                SET {addiction} = '{str(time_now)}'
+                                    WHERE id = {user_id};
+                            '''
+            methods.basic_commit(update_query)
+            return True
+        #already has addiction
+        return False
+           
+
+##
+    def fetch_addiction(self,user_id,addiction):
+        '''
+            fetches specific addiction start time
+            return format is '%Y-%m-%d %H:%M:%S.%f'
+        '''
+
+        query = f'''
+                    SELECT {addiction} FROM user_addictions
+                        WHERE id = {user_id};
+                '''
+        res = methods.basic_fetch(query, 'one')[0]
+        return res
+
+
+
+    def convert_str_time_to_datetime(self,str_time):
+        '''
+            converts time in string to datetime object
+            return format '%Y-%m-%d %H:%M:%S.%f'
+
+        '''
+
+        time_format = '%Y-%m-%d %H:%M:%S.%f'
+        time = dt.datetime.strptime(str_time,time_format)
+        return time
+
+
+##
+    def fetch_user_addictions(self,user_id):
+        '''
+            fetches user's current addictions
+            returns [ [addiction1, [d,h,m,s]], [addiction2, [d,h,m,s]].. ]
+        '''
+
+        valid = ''
+        for i in valid_addictions:
+            valid +=f'{i}, '
+        query = f'''
+                    SELECT {valid[:-2]} FROM user_addictions
+                        WHERE id = {user_id};
+                '''
+        addiction_start_times = methods.basic_fetch(query,'one')
+        addiction_and_delta_times = []
+        for time in zip(valid_addictions,addiction_start_times):
+            if time[1] is not None:
+                deltatime = self.time_diff_results(user_id,time[1])
+                addiction_and_delta_times.append([time[0],self.convert_dt_to_list(deltatime)])
+        return addiction_and_delta_times
+
+
+
+    def convert_dt_to_list(self,date):
+        '''
+            converts a time difference 'd day, h:m:s' 
+            into a list to access, returns [d,h,m,s]
+        '''
+
+        values = str(date).split()
+        days = 0
+        all_values = values[-1].split(':')
+        all_values[-1] = all_values[-1][:2]
+        print(all_values)
+        for i in range(1,3):
+            if all_values[i][0] == '0':
+                all_values[i] =  all_values[i][1]
+        print(all_values)
+        if len(values)>1:
+            days = values[0]
+        all_values.insert(0, days)
+        return all_values
+
+
+
+##
+    def remove_addiction(self,user_id,addiction):
+        '''
+            remove addiction from user account
+            returns datetime list by self.convert_dt_to_list(date)
+            or False
+        '''
+
+        diff = self.addiction_time_results(user_id,addiction=addiction)
+        if bool(diff):
+            #diff format: x days, h:m:s.f
+            rep_time = diff.total_seconds()
+            dt_in_list = self.convert_dt_to_list(diff)
+            query = f'''
+                        UPDATE user_addictions 
+                            SET {'s'+addiction} = array_append({'s'+addiction},{rep_time}), {addiction} = null
+                                 WHERE id = {user_id};
+                    '''
+            methods.basic_commit(query)
+            return dt_in_list
+        return False
+
+
+
+    def addiction_time_results(self,user_id,addiction=False):
+        '''
+            returns time results after user resets or removes addiction
+        '''
+
+        start = self.fetch_addiction(user_id,addiction) 
+        if start is not None:
+            deltatime = self.time_diff_results(user_id, start)
+            return deltatime
+        return False
+
+
+
+    def time_diff_results(self,user_id,start):
+        '''
+            takes in str_time and converts to datetime
+            returns deltatime 'x days, h:m:s.f'
+        '''
+
+        end = dt.datetime.now()
+        start = self.convert_str_time_to_datetime(start)
+        return end-start
+        
+
+
+
+
+
+adic = addictions()
---
+++ apis/user.py
@@ -0,0 +1,51 @@
+import datetime as dt
+from db.methods import methods
+
+
+class user_data:
+
+    def initialize(self, user_id):
+        '''
+            Initializes an account for user
+        '''
+
+        is_user = self.is_user(user_id)
+        if not is_user:
+            query1 =f'''
+                            INSERT INTO user_info(id)
+                                VALUES({user_id})
+                        '''
+            query2 =f'''
+                            INSERT INTO user_addictions(id)
+                                VALUES({user_id})
+                        '''
+            query3 = f'''
+                            INSERT INTO user_routines(id)
+                                VALUES({user_id})
+                        '''
+            methods.basic_commit([query1,query2,query3])
+            return True
+        #if user already has account
+        return False
+
+        
+
+    def is_user(self,user_id):
+        '''
+            check if user is in db
+            returns a bool
+        '''
+
+        query = f'''
+                    SELECT EXISTS(SELECT 1 FROM user_info 
+                        WHERE id = {user_id}) 
+                '''
+        res = methods.basic_fetch(query,'one')[0]
+        return res
+
+
+
+
+
+user = user_data()
+
